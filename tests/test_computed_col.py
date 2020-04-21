b556c46e052b0f2f68e07d395ee02c399579d52f
Gordon Watts <gordon@gordonandpaula.net>
2020-04-21 02:48:18 -0700

    Add computed column tests
    To make sure we are getting the objects and references right.

--- tests/test_computed_col.py
+++ tests/test_computed_col.py
@@ -1,8 +1,9 @@
 import ast
+from dataframe_expressions.asts import ast_Callable
 
 import pytest
 
-from dataframe_expressions import DataFrame
+from dataframe_expressions import DataFrame, user_func
 
 from .utils_for_testing import reset_var_counter  # NOQA
 
@@ -107,3 +108,59 @@ def test_create_col_with_lambda():
     assert p.parent is not None
     assert isinstance(p.parent, DataFrame)
     assert p.parent is df
+
+    assert isinstance(d1.child_expr.func, ast_Callable)
+    assert d1.child_expr.func.dataframe is p
+
+
+def test_col_twice_nested():
+    df = DataFrame()
+    df.jets['ptgev'] = lambda j: j.pt / 1000.0
+    good_jets = df.jets[df.jets.pt > 35]
+    d1 = good_jets.ptgev
+
+    # Make sure correct df is referenced here
+    assert d1.child_expr is not None
+    assert "Call(func=ast_Callable(name='lambda" in ast.dump(d1.child_expr)
+    assert d1.filter is None
+
+    assert d1.parent is not None
+    assert d1.parent.filter is not None
+
+
+def test_nested_col_access():
+    df = DataFrame()
+
+    @user_func
+    def DeltaR(p1_eta: float) -> float:
+        '''
+        Calculate the DeltaR between two particles given their `eta` and `phi` locations.
+        Implemented on the back end.
+        '''
+        assert False, 'This should never be called'
+
+    mc_part = df.TruthParticles('TruthParticles')
+    eles = df.Electrons('Electrons')
+
+    def dr(e, mc):
+        'Make calculating DR easier as I have a hard-to-use DR calculation function on the back end'
+        return DeltaR(e.eta())
+
+    def very_near2(mcs, e):
+        'Return all particles in mcs that are DR less than 0.5'
+        return mcs[lambda m: dr(e, m) < 0.1]
+
+    eles['near_mcs'] = lambda reco_e: very_near2(mc_part, reco_e)
+
+    eles['hasMC'] = lambda e: e.near_mcs.Count() > 0
+    good_eles_with_mc = eles[eles.hasMC]
+    good_eles_with_mc['mc'] = lambda e: e.near_mcs.First().ptgev
+
+    d1 = good_eles_with_mc.mc
+
+    assert d1.filter is None
+    assert d1.child_expr is not None
+    assert isinstance(d1.child_expr, ast.Call)
+    assert isinstance(d1.child_expr.func, ast_Callable)
+
+    assert d1.child_expr.func.dataframe is d1.parent
